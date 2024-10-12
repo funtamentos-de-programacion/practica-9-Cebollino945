df7956374c3a07c8e90f15d5debcb541447aebef
Cebollino945 <angelhergonzalez0@gmail.com>
2024-10-11 20:56:50 -0600

    add:codigos resueltos

--- arreglos4.c
+++ arreglos4.c
@@ -5,17 +5,17 @@
 //  id: suma
 //  tipo de retorno int
 //  argumento un apuntador a entero y un entero
-
+int suma(int*, int);
 
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
 
     // TODO: (11) Crea una variable llamada resultado  y asigna lo que devuelve la función suma
     // pasa a la función el arreglo y su tamaño
-    
+    int resultado = suma(arr, 5);
     
     // TODO: (12) Imprime el resultado
-    
+    printf("%d", resultado);
     return 0;
 }
 
