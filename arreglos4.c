f26ce9e2009d6159c2258750f7ac529cbd6057e8
Cebollino945 <angelhergonzalez0@gmail.com>
2024-10-17 10:44:04 -0600

    Solucion de los ejercicios del 6 al 9

--- arreglos4.c
+++ arreglos4.c
@@ -5,10 +5,11 @@
 //  id: suma
 //  tipo de retorno int
 //  argumento un apuntador a entero y un entero
-int suma(int*, int);
+int suma(int*ptr, int resultado);
 
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
+    
 
     // TODO: (11) Crea una variable llamada resultado  y asigna lo que devuelve la función suma
     // pasa a la función el arreglo y su tamaño
@@ -21,4 +22,15 @@ int main() {
 
 
 //TODO: (13) Crea la función suma que realiza la suma de todos los elementos de un arreglo
-
+int arr[5] = {1, 2, 3, 4, 5};
+int suma(int *ptr, int resultado){
+ptr = arr;
+for(int i= 0; i<5; i++){
+    printf("%d, ", *(ptr +i) );
+}
+}
+int calcularsuma(int ptr[], int 5){
+    int sumas = 0;
+for (int i= 0; i<5; i++){
+    sumas += ptr[i];
+}
--- arreglos9.c
+++ arreglos9.c
@@ -4,21 +4,28 @@ int main() {
     int A[2][2] = {{1, 2}, {3, 4}};
     int B[2][2] = {{5, 6}, {7, 8}};
     int C[2][2] = {0};  // Matriz resultante
+
+    int *ptrA, *ptrB, *ptrC;
+    ptrA = &A[0][0];
+    ptrB = &B[0][0];
+    ptrC = &C[0][o];
     
     // Multiplicación de matrices usando punteros
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
             for (int k = 0; k < 2; k++) {
+                //C[i][j] += A[i][k]+B[k][j]
                 // TODO: Realiza las operaciones solo usando apuntadores
+                *(ptrC+i*2+j) += *(ptrA+i*2+k) * *(ptrB+k+j*2);
             }
         }
     }
     
     // Mostrar matriz resultante
     for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            // TODO: Imprime los elementos de la matriz resultante, usando apuantadores
-        }
+        printf("%d\t",*(ptrC + i) );
+        ((i)%3==2)? printf("\n"):printf("") ;
+       
         
     }
     
